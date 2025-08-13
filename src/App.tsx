5670645aaa69c35b784109c0e200d3f06f23074d
OmKumar07 <omkumarishere@gmail.com>
2025-08-13 22:38:01 +0530

    Basic_NavBar_implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar.js";
 
 function App() {
   return (
@@ -9,8 +10,8 @@ function App() {
             "aside main"`,
       }}
     >
-      <GridItem area="nav" bg="blue.500">
-        Navigation
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       <GridItem
         area="aside"
