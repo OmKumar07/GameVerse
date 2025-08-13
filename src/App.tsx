cb29799ae576591da2734031c6f5f991f17d3953
OmKumar07 <omkumarishere@gmail.com>
2025-08-13 17:05:50 +0530

    Responsive_Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 function App() {
-  return <Button>Click me</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav""main"`,
+        lg: `"nav nav"
+            "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="blue.500">
+        Navigation
+      </GridItem>
+      <GridItem
+        area="aside"
+        bg="gold"
+        p={4}
+        display={{ base: "none", lg: "block" }}
+      >
+        Sidebar
+      </GridItem>
+      <GridItem area="main" bg="coral">
+        Main Content
+      </GridItem>
+    </Grid>
+  );
 }
+
 export default App;
